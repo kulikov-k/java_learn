ab9ff144eb76f37c6f36f01b99c2dff3f4eadb43
kulikov-k <kostysk@yandex.ru>
2025-07-10 16:50:53 +0300

    commit

---
+++ t4.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
+который запишет эту строку в простой текстовый файл, обработайте исключения.
+*/
+public class t4 {
+    public static void main(String[] args) {
+        String testString = buildTestString();
+        String filename = "output.txt";
+        writeToFile(testString, filename);
+    }
+
+    public static String buildTestString() {
+        StringBuilder sb = new StringBuilder(4 * 100);
+        for (int i = 0; i < 100; i++) {
+            sb.append("TEST");
+        }
+        return sb.toString();
+    }
+
+    public static void writeToFile(String content, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+            System.out.println("Запись в файл успеешно заверщена: " + filename);
+        } catch (IOException e) {
+            System.err.println("Произошла ошибка при записи в файл : " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
