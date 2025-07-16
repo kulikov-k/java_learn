c8ef75c0638ea59b5d377708ab82dd0099df6a85
kulikov-k <kostysk@yandex.ru>
2025-07-16 16:46:18 +0300

    commit

---
+++ dz_3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class dz_3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите Ваше имя: ");
+        String name = scanner.nextLine();
+        scanner.close();
+        System.out.println("Отлично," + name);
+    }
+}
