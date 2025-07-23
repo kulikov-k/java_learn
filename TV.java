6e427c02cc664ad0244ea1e7284520ebbe4b1548
kulikov-k <kostysk@yandex.ru>
2025-07-23 10:35:41 +0300

    commit

---
+++ TV.java
@@ -0,0 +1,37 @@
+    public class TV {
+        // Приватные поля
+        private String производитель;
+        private int размер; // в дюймах
+        private boolean включен;
+
+        // Конструктор
+        public TV (String производитель, int размер) {
+            this.производитель = производитель;
+            this.размер = размер;
+            this.включен = false; // по умолчанию выключен
+        }
+
+        // Геттеры и сеттеры (свойства)
+        public String getПроизводитель() {
+            return производитель;
+        }
+        public int getРазмер() {
+            return размер;
+        }
+
+        public boolean isВключен() {
+            return включен;
+        }
+
+        // действия (управление)
+        public void включить() {
+            включен = true;
+            System.out.println("Телевизор " + производитель + " включен.");
+        }
+
+        public void выключить() {
+            включен = false;
+            System.out.println("Телевизор " + производитель + " выключен.");
+        }
+        }
+
